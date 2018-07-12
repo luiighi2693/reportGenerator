2cf8ec5c5b008b5c403af5812eb915bcd208bad1
user <orlandop@greensize.net>
2018-07-12 16:13:04 -0400

    first commit

---
+++ src/test/java/com/greensize/develop/reportGenerator/ReportGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greensize.develop.reportGenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
