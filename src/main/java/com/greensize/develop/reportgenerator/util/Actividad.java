e948f758bbd928a29cddf63ae2fe4d759924fd8d
user <orlandop@greensize.net>
2018-07-18 12:26:56 -0400

    Enum Actividad, Cliente y TipoHorario

---
+++ src/main/java/com/greensize/develop/reportgenerator/util/Actividad.java
@@ -0,0 +1,9 @@
+package com.greensize.develop.reportgenerator.util;
+
+/**
+ * Created by greensize-backend on 18/7/2018.
+ */
+public enum Actividad {
+    SOPORTE,
+    DESARROLLO
+}
---
+++ src/main/java/com/greensize/develop/reportgenerator/util/TipoHorario.java
@@ -0,0 +1,12 @@
+package com.greensize.develop.reportgenerator.util;
+
+/**
+ * Created by greensize-backend on 18/7/2018.
+ */
+public enum TipoHorario {
+    NORMAL,
+    EXTRAS_NORMAL,
+    NOCTURNAS,
+    FIN_DE_SEMANA,
+    FERIADO_USA
+}
